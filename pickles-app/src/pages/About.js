26dd27660ce83278c324216b681c6ebf71139fc6
lora lameh <loralameh@gmail.com>
2022-02-26 18:12:58 +0200

    create  about page/ add staff component to about page

--- pickles-app/src/pages/About.js
+++ pickles-app/src/pages/About.js
@@ -1,18 +1,29 @@
 import React from 'react'
-
+import Staff from '../components/Staff'
 const About = () => {
   return (
-    <section className='section'>
-      <h1>about us</h1>
-      {/* slider here */}
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-        repudiandae architecto qui adipisci in officiis, aperiam sequi atque
-        perferendis eos, autem maiores nisi saepe quisquam hic odio consectetur
-        nobis veritatis quasi explicabo obcaecati doloremque? Placeat ratione
-        hic aspernatur error blanditiis?
-      </p>
-    </section>
+    <>
+      <section className='section'>
+        <div className='title'>
+          <h2>
+            Who is <em> ..Pickley.. </em>?
+          </h2>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          repudiandae architecto qui adipisci in officiis, aperiam sequi atque
+          perferendis eos, autem maiores nisi saepe quisquam hic odio
+          consectetur nobis veritatis quasi explicabo obcaecati doloremque?
+          Placeat ratione hic aspernatur error blanditiis?
+        </p>
+      </section>
+      <section className='section'>
+        <div className='title'>
+          <h2>Meet our staff</h2>
+        </div>
+        <Staff />
+      </section>
+    </>
   )
 }
 
