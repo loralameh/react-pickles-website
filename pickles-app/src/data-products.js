3eb92f2c73db44a7b0510c7ad232c56c213027b8
lora lameh <loralameh@gmail.com>
2022-02-26 21:59:04 +0200

    add data file

---
+++ pickles-app/src/data-products.js
@@ -0,0 +1,36 @@
+const products = [
+  {
+    id: 1,
+    name: 'Pickled Cucumber',
+    price: 3.5,
+    image:
+      'https://images.pexels.com/photos/9462571/pexels-photo-9462571.jpeg?cs=srgb&dl=pexels-%D0%BB%D0%B5%D1%81%D1%8F-%D1%82%D0%B5%D1%80%D0%B5%D1%85%D0%BE%D0%B2%D0%B0-9462571.jpg&fm=jpg',
+    text: ' meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry',
+  },
+  {
+    id: 2,
+    name: 'Pickled Olives',
+    price: 5,
+    image:
+      'https://images.pexels.com/photos/5975610/pexels-photo-5975610.jpeg?cs=srgb&dl=pexels-eva-elijas-5975610.jpg&fm=jpg',
+    text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+  },
+  {
+    id: 3,
+    name: 'Pickled Tomatoes',
+    price: 10.2,
+    image:
+      'https://images.pexels.com/photos/5975654/pexels-photo-5975654.jpeg?cs=srgb&dl=pexels-eva-elijas-5975654.jpg&fm=jpg',
+    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'Pickled Garlic',
+    price: 7,
+    image:
+      'https://images.pexels.com/photos/5975762/pexels-photo-5975762.jpeg?cs=srgb&dl=pexels-eva-elijas-5975762.jpg&fm=jpg',
+    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+]
+
+export default products
