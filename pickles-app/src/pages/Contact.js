d0a9e291a5084e0b2a83867bf52652061279420c
lora lameh <loralameh@gmail.com>
2022-02-27 00:50:22 +0200

    add contact page

---
+++ pickles-app/src/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReviewForm from '../components/ReviewForm'
+const Contact = () => {
+  return (
+    <>
+      <br />
+      <h2>Want to tell us something?</h2>
+      <h3>We love to hear from you</h3>
+      <ReviewForm />
+    </>
+  )
+}
+
+export default Contact
