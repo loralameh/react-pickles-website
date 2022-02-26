730dfdad16f8b4a8e60c5d60148507dee91c9473
lora lameh <loralameh@gmail.com>
2022-02-26 21:56:41 +0200

    add a header image

--- pickles-app/src/pages/Home.js
+++ pickles-app/src/pages/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import Products from '../components/Products'
 const Home = () => {
   return (
     <div>
+      <div className='section'>
+        <div className='section-center'>
+          <img
+            src='https://images.pexels.com/photos/6611594/pexels-photo-6611594.jpeg?cs=srgb&dl=pexels-dale-jackson-6611594.jpg&fm=jpg'
+            alt='header'
+          />
+        </div>
+      </div>
+      <Products />
       {/* slider for products */}
       {/* slider for reviews */}
       {/* form to add a review */}
