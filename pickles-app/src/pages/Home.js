14d0f08f5c2d5b95cb2d29bcb4e464105f53965c
lora lameh <loralameh@gmail.com>
2022-03-11 19:56:53 +0200

    fix image style

--- pickles-app/src/pages/Home.js
+++ pickles-app/src/pages/Home.js
@@ -2,17 +2,16 @@ import React from 'react'
 import Products from '../components/Products'
 const Home = () => {
   return (
-    <div>
-      <div className='section'>
-        <div className='section-center'>
-          <img
-            src='https://images.pexels.com/photos/8879387/pexels-photo-8879387.jpeg?cs=srgb&dl=pexels-ron-lach-8879387.jpg&fm=jpg'
-            alt='header'
-          />
-        </div>
+    <>
+      <div className='hero-cont'>
+        <img
+          className='hero-image'
+          src='https://images.pexels.com/photos/8879387/pexels-photo-8879387.jpeg?cs=srgb&dl=pexels-ron-lach-8879387.jpg&fm=jpg'
+          alt='header'
+        />
       </div>
       <Products />
-    </div>
+    </>
   )
 }
 
